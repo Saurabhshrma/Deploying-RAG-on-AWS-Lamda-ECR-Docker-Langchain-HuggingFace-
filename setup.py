ccd48f33c601ee0ce756762b4fb786c21a0b5a33
Saurabhshrma <saurabh1346.ss@gmail.com>
2024-08-19 20:15:00 +0530

    req update

--- setup.py
+++ setup.py
@@ -6,10 +6,17 @@ setup(
     author="Saurabh",
     author_email="saurabh1346.ss@gmail.com",
     packages=find_packages(),
-    install_requires=["langchain","langchainhub","bs4","tiktoken","openai","boto3","langchain_community","chromadb","awscli",
-"streamlit",
-"pypdf",
-"faiss-cpu",
-"langchain-aws"]
+    install_requires=[
+        "langchain==0.2.14",
+        "langchainhub==0.1.21",  
+        "beautifulsoup4==4.12.3",
+        "tiktoken==0.7.0",
+        "boto3==1.34.37",
+        "langchain-community==0.2.12",
+        "awscli==2.17.32",
+        "streamlit==1.37.0",
+        "pypdf==3.0.1",
+        "faiss-cpu==1.8.0.post1",
+        "langchain-aws==0.1.16"
+    ]
 )
-    
